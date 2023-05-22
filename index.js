0874debb7e7ccba493d672f10fd0b456a5db008d
Ubeydu <u.keles@gmail.com>
2023-05-22 19:00:11 +0300

    Changed greeting.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({bye: 'there'});
+    res.send({bye: 'Ubi'});
 })
 
 const PORT = process.env.PORT || 5000;
