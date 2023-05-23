5a83510a6cc66f0c26c52d74db1bd40460ed4745
Ubeydu <u.keles@gmail.com>
2023-05-23 18:07:51 +0300

    Created and hooked up mongodb db.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     googleClientID: '887192575160-q1vkcomiaecp0rhq5e3rfjai7v5i5a2j.apps.googleusercontent.com',
     goolgeClientSecret: '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',
+    mongoURI: 'mongodb+srv://ukeles: NuhEQhpsSQZMJUaW@cluster0.v6wcuif.mongodb.net/eamilydb?retryWrites=true&w=majority'
 };
