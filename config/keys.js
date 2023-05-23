ad95e4014ee271b3abba3d2a8401dd518642b8b1
Ubeydu <u.keles@gmail.com>
2023-05-23 11:53:18 +0300

    Cont. setting up google auth.

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: '887192575160-q1vkcomiaecp0rhq5e3rfjai7v5i5a2j.apps.googleusercontent.com',
+    goolgeClientSecret: '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',
+};
