46fbb68af7aa8fd0e94b044c8f93fa6717541c98
Ubeydu <u.keles@gmail.com>
2023-05-24 12:01:28 +0300

    Improved user create process by adding done arg.

--- services/passport.js
+++ services/passport.js
@@ -16,9 +16,12 @@ passport.use(
             User.findOne({ googleId: profile.id }).then(existingUser => {
                 if (existingUser) {
                     // We already have a record with the given profile ID
+                    done(null, existingUser);
                 } else {
                     // We don't have a user record with this profile ID, create a new record.
-                    new User({ googleId: profile.id}).save();
+                    new User({ googleId: profile.id})
+                        .save()
+                        .then(user => done(null, user));
                 }
             });
         }
