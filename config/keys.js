ee4ca563ce5fffd17286f90476f70d16c733628c
Ubeydu <u.keles@gmail.com>
2023-05-24 15:12:28 +0300

    Added cookieKey.

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
     googleClientID: '887192575160-q1vkcomiaecp0rhq5e3rfjai7v5i5a2j.apps.googleusercontent.com',
     goolgeClientSecret: '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',
-    mongoURI: 'mongodb+srv://ukeles:NuhEQhpsSQZMJUaW@cluster0.v6wcuif.mongodb.net/eamilydb?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://ukeles:NuhEQhpsSQZMJUaW@cluster0.v6wcuif.mongodb.net/eamilydb?retryWrites=true&w=majority',
+    cookieKey: 'fhdnfdksjnkfsdnfvkldnflvkgndlfkgndlfkvnlfdknld'
 };
