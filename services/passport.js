b0ce0658cb64133ad34e9f6065cbae025b61076f
Ubeydu <u.keles@gmail.com>
2023-05-24 15:56:46 +0300

    Error fix attempt.

--- services/passport.js
+++ services/passport.js
@@ -6,13 +6,13 @@ const keys = require('../config/keys');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+    return done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
     User.findById(id)
         .then(user => {
-            done(null, user);
+            return done(null, user);
         })
 });
 
